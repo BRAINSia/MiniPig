c6bf2f9cb0a2c4688e0b87e1239418e5c6f6863e
schubisu <robin.schubert@ghi-muenster.de>
2015-01-29 15:31:57 +0100

    added some comments in python scripts

--- scripts/008_RenameRawData.py
+++ scripts/008_RenameRawData.py
@@ -1,11 +1,60 @@
+import os
+import shutil
 
-names = {
-        "Charlotte", "Diana", "Kate", "Lukrezia", "Mary", "Stuart", "Wu", "Zetian",
-        "Ada", "Beatrix", "Katharina", "Katarina", "Letizia", "Leticia", "Maxima",
-        "Kamilla", "Fergie", "Grace", "Kelly", "Hedwig", "Silvia", "Victoria", "Viktoria",
-        "Charlene", "Charlenne", "Cleopatra", "Elli", "Else", "Luise",
-        "Adelheid", "Marie", "Nitokris", "Pippa", "Sissi", "Sophie",
-        "Agrippina", "Juliana", "Leopoldine", "Mette", "Marit", "Theo", "Nofretete", 
-        "-", "Clear", "Clea"}
+# local origin
+path = "/var/mri/raw/"
+filenames = os.listdir(path)
 
+# separate filenames in basename
+basenames = [item.split(".")[0] for item in filenames]
+# extension
+endings = [".".join(item.split(".")[1:]) for item in filenames]
+# date
+dates = [item[:8] for item in filenames]
+# earmarks
+earmarks = [item[-6:-3] for item in basenames]
+
+# lisf of names that have to be deleted from filename
+names = [
+         "Charlotte", "Diana", "Kate", "Lukrezia", "Lucrezia", "Lucrez", "Mary", "Stuart", "Wu", "Zetian",
+         "Ada", "Beatrix", "Katharina", "Katarina", "Letizia", "Leticia", "Maxima",
+         "Camilla", "Kamilla", "Fergie", "Grace", "Kelly", "Hedwig", "Silvia", "Victoria", "Viktoria",
+         "Charlene", "Charlenne", "Cleopatra", "Elli", "Else", "Luise",
+         "Adelheid", "Marie", "Nitokris", "Pippa", "Sissi", "Sophie",
+         "Agrippina", "Juliana", "Leopoldine", "Mette", "Marit", "Theo", "Nofretete",
+         "-", "CLEAR", "CLEA", "LEAR", "LEA", "CLE"] 
+
+# clumsy approach to delete unwanted strings from filename
+basenames2 = []
+for base in basenames:
+    for name in names:
+        base = base.replace(name, "")
+    basenames2 += [base]
+
+# extract sequence names
+methods = [item[15:-6] for item in basenames2]
+# put every part of the new file name in a tuple list
+variables = zip(earmarks, dates, methods, endings)
+
+# create new filenames
+newnames = ["M{0}P100_{1}_{2}.{3}".format(a, b, c, d) for a, b, c, d in variables]
+# create new path
+newFullPath = [os.path.join("/home/hans/MINIPIG_HD_100/M{0}P100/M{0}P100_{1}/M{0}P100_{1}_{2}.{3}".format(a, b, c, d)) for a, b, c, d in variables]
+
+
+bothnames = zip(filenames, newFullPath)
+newpath = "/home/hans/MINIPIG_HD_100"
+newFullPath
+
+# do the renaming
+for src, dst in bothnames:
+    try:
+        os.mkdirs(os.path.dirname(dst))
+        print('created directory {0}'.format(os.path.dirname(dst)))
+    except:
+        print('directory {0} already existed'.format(os.path.dirname(dst)))
+        pass
+    shutil.copy2(os.path.join(path, src), dst)
+    
+#[shutil.copy2(os.path.join(path, src), os.path.join(newpath, dst)) for src, dst in bothnames]
 
--- scripts/009_TransformNifti.py
+++ scripts/009_TransformNifti.py
@@ -1,16 +1,27 @@
+# applying a cosine transform to the direction
+# matrix of Nifti images. Swapping A->P with I->S
+
 import numpy as np
 import sys
+
+# define transformation matrix
 transform_matrix = np.matrix('1 0 0 0; 0 0 1 0; 0 -1 0 0; 0 0 0 1')
 
+# transformation in separate method
 def transform_nifti(path, transformation):
     import nibabel
     import numpy as np
 
+    # load nifti
     image = nibabel.load(path)
+    # get direction matrix
     affine = image.get_affine()
+    # multiply with transformation matrix
     affine = affine.dot(transformation)
+    # set result as qform / sform
     image.set_qform(affine)
     image.set_sform(affine)
+    # save nifti, overwrites original!
     nibabel.save(image, path)
 
 transform_nifti(sys.argv[1], transform_matrix)
